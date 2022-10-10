6feff2d5335810f69beef237d9bd6c3c5ee33109
WikH520 <115477435+WikH520@users.noreply.github.com>
2022-10-10 22:23:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,22 +19,20 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '韩佳慧',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oS0kq65_ft2r-WkOg8iAsbtIuZdw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'rFgVylJoEO0w-mrR-Wvdaj-O9W1SXJt5ggyNq7t9D9M',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '01-23',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '韩佳慧', year: '2004', date: '01-23',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
+        
         {
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
         },
