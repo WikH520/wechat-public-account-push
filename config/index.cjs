c43172fb41221a18633e6cc6d167732bfb6db196
WikH520 <115477435+WikH520@users.noreply.github.com>
2022-10-10 21:41:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,22 +8,22 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx10e63668b7c18eba',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'c8a324facd73653c84d8f6c7c35d0968',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '河南',
+  CITY: '郑州',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oS0kq65_ft2r-WkOg8iAsbtIuZdw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'rFgVylJoEO0w-mrR-Wvdaj-O9W1SXJt5ggyNq7t9D9M',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
